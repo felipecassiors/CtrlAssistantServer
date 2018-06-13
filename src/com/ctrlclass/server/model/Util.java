18767e18731e53291824b928c2c3af39130379a9
felipecassiors <felipe.santos@gec.inatel.br>
2018-06-13 00:39:53 -0300

    Adjusted duration formatter

--- src/com/ctrlclass/server/model/Util.java
+++ src/com/ctrlclass/server/model/Util.java
@@ -10,7 +10,7 @@ public abstract class Util {
         long seconds = duration.getSeconds();
         long absSeconds = Math.abs(seconds);
         String positive = String.format(
-                "%d:%02d:%02d",
+                "%02d:%02d:%02d",
                 absSeconds / 3600,
                 (absSeconds % 3600) / 60,
                 absSeconds % 60);
